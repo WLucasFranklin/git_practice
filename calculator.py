f5009c2170b8b3477ce1dbb4252b3362d9ad37f2
wlucasfranklin <wlucasfranklin@gmail.com>
2026-05-04 13:33:38 -0400

    Multiplication

--- calculator.py
+++ calculator.py
@@ -10,4 +10,7 @@ if __name__ == "__main__":
     print("Calculator loaded!")
     print(f"5 + 3 = {add(5, 3)}")
     print(f"10 - 4 = {subtract(10, 4)}")
+
+def multiply(a, b):
+    return a * b
 EOF
